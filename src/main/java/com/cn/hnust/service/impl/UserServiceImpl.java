c5ffbefca0c46427fa93e3e75885aba4450df994
MoPuSam <13968952370@163.com>
2018-01-12 11:42:28 +0800

    the second modify

--- src/main/java/com/cn/hnust/service/impl/UserServiceImpl.java
+++ src/main/java/com/cn/hnust/service/impl/UserServiceImpl.java
@@ -4,8 +4,6 @@ import javax.annotation.Resource;
 
 import org.springframework.stereotype.Service;
 
-import com.cn.hnust.dao.IUserDao;
-import com.cn.hnust.pojo.User;
 import com.cn.hnust.service.IUserService;
 
 @Service("userService")
